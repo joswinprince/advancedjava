1fbf146c4c305a4b6a1539355d6a81b6110ded79
joswinprince <kingdomjoswin@gmail.com>
2022-07-11 12:05:42 +0530

    Iteratosysout

--- GenericMethods.java
+++ GenericMethods.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 /**
 *
@@ -11,17 +12,26 @@ static Integer [] intArray = {1, 2, 3, 4, 5};
 static Boolean [] boolArray = {true, false, true };
 
 
-public static List arrayToList(Object[] array, List<Object> list) {
-for (Object object : array) {
+public static<T> List<T> arrayToList(T[] array, List<T> list) {
+for (T object : array) {
 list.add(object);
 }
 return list;
 }
+
 public static void main(String[] args) {
     List<Character> charList = arrayToList (charArray, new ArrayList<> ());
     List<Boolean> boolList=arrayToList(boolArray, new ArrayList<>());
     List<Integer>intList = arrayToList(intArray, new ArrayList<>());
-
-    System.out.println(intList.get(0));
+    //Iterating using the Iterator java
+    Iterator itr =intList.iterator();
+    
+    while(itr.hasNext())
+    {
+        //Passing values from the Iterator
+    Object element = itr.next();
+    System.out.println(element+" ");
+    }
+    
     }
 }
